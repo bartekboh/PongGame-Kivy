59e1a747e55226886901d2c563d61013d35f123d
bartekboh <bartekbohdziewicz@gmail.com>
2023-05-10 20:19:54 +0200

    Added boosts - still need to work on gravity upgrade {Duration timer and check if it wont work only for one player}

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from kivy.app import App
@@ -72,6 +73,7 @@ class PongBall(Widget):
 
 class PongGame(Widget):
     menu_widget = ObjectProperty()
+    start_widget = ObjectProperty()
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
@@ -89,18 +91,29 @@ class PongGame(Widget):
 
         self.refresh_rate = 90
 
-        self.menu_state = True
+        self.start_state = True
+        self.menu_state = False
         self.game_over_state = False
         self.game_over_score = 10
-
+        
         self.stars_count = random.randrange(30, 60)
         self.stars_list = []
         self.init_star()
 
-        self.boost_timer = 0
-
+        self.ball_speed = 6
         self.paddle_speed = 6
 
+        self.org_ball_w = self.ball.width
+        self.org_ball_h = self.ball.height
+        self.org_paddle_h = self.player1.height
+        self.org_ball_speed = self.ball_speed
+        self.org_paddle_speed = self.paddle_speed
+
+        self.gravity_boost = False
+
+        self.boost_timer = 0
+
+        self.boost_type = ""
         self.avaiable_boosts = ["ball_speed", "paddle_size", "ball_size", "gravity"]
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
@@ -113,24 +126,19 @@ class PongGame(Widget):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
-
     def _on_keyboard_down (self, keyboard, keycode, text, modifiers):
-        #pressed_key = self._keyboard.keycode_to_string(keycode) # this does not work somehow
         pressed_key = keycode[1]
-        print('You pressed the key', pressed_key, '.', sep=' ', end='\n')
 
         self.pressed_keys[pressed_key] = True
 
         if pressed_key == 'escape':
             print("ESC")
             self.change_menu_state()
-        
 
         return True
 
     def _on_keyboard_up (self, keyboard, keycode):
         released_key = keycode[1]
-        print('You released the key', released_key, '.', sep=' ', end='\n')
         self.pressed_keys[released_key] = False
         return True
 
@@ -158,10 +166,6 @@ class PongGame(Widget):
             self.stars_list[i].points = [star_x, star_y, star_x, star_y]
             self.stars_list[i].width = star_w
 
-    # BOOSTS
-    def on_ball_speed_boost(self):
-        self.ball.velocity_x *= 1.1
-
     def on_gravity(self):
         # for red
         self.ball.velocity_x += 0.1
@@ -169,18 +173,48 @@ class PongGame(Widget):
         # for blue
         self.ball.velocity_x -= 0.1
 
-    def serve_ball(self, vel=(4, 0)):
+    def serve_ball(self, vel=(6, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
+    def detect_collision_with_boost(self):
+        ba = self.ball
+        bo = self.boost
+        ba_r = ba.width/2
+        bo_r = bo.width/2
+
+        diff_x = abs(ba.center_x - bo.center_x)
+        diff_y = abs(ba.center_y - bo.center_y)
+
+        dis = math.sqrt(math.pow(diff_x, 2) + math.pow(diff_y, 2))
+
+        if dis <= ba_r + bo_r:
+            print("Removing boost: ", dis, "<", ba_r, "+", bo_r)
+            self.remove_boost_widget()
+
+            return True
+        return False
+    
+
+    def remove_boosts(self):
+        self.ball.width = self.org_ball_w
+        self.ball.height = self.org_ball_h
+        self.player1.height = self.org_paddle_h
+        self.player2.height = self.org_paddle_h
+        self.ball_speed = self.org_ball_speed
+        self.ball.velocity_x = self.ball_speed
+        self.paddle_speed = self.org_paddle_speed
+
+
     def update(self, dt):
 
-        
+        if not self.menu_state:
+            self.menu_widget.opacity = 0
 
         if self.pressed_keys == 'escape':
             print("ESC")
 
-        if self.game_over_state or self.menu_state:
+        if self.game_over_state or self.menu_state or self.start_state:
             self.serve_ball(vel=(0, 0))
             # self.game_over_screen
             self.player1.center_y = self.height * 0.9 / 2
@@ -192,21 +226,46 @@ class PongGame(Widget):
 
             if self.menu_state:
                 pass
+            
 
         else:
             self.ball.move()
 
+            # ADD GRAVITY DURATION TIMER
+            if self.gravity_boost:
+                self.ball.velocity_x += 0.5
+         
+            if self.detect_collision_with_boost():
+                if self.boost_type == "ball_speed":
+                    if not self.ball.velocity_x >= 12:
+                        self.ball.velocity_x *= 1.3
+                elif self.boost_type == "paddle_size":
+                    if not self.player1.height >= self.org_paddle_h * 1.5:
+                        self.player1.height *= 1.3
+                        self.player2.height *= 1.3
+                        self.paddle_speed *= 0.8
+                elif self.boost_type == "ball_size":
+                    if not self.ball.width >= self.org_ball_w * 2 and not self.ball.height >= self.org_ball_h * 2:
+                        self.ball.width *= 1.3
+                        self.ball.height *= 1.3
+                elif self.boost_type == "gravity":
+                    if not self.ball.velocity_x >= self.ball.velocity_x * 1.1:
+                        self.gravity_boost = True
+                
+
+
             if self.boost_timer > 0:
                 self.boost_timer -= dt
-                print(self.boost_timer)
             
             else:
                 self.remove_boost_widget()
 
             if random.randrange(200) == 5 and self.boost_timer == 0:
                 self.boost_timer = random.randrange(20,60)
-                self.boost.pos_x = random.randrange(int(self.width))
+                self.boost.pos_x = random.randrange(int(self.width/8), int(self.width*(7/8)))
                 self.boost.pos_y = random.randrange(int(self.height * 0.8))
+                self.boost_type = self.avaiable_boosts[int(random.randrange(4))]
+                print(self.boost_type)
 
             # bounce of paddles
             self.player1.bounce_ball(self.ball)
@@ -215,21 +274,24 @@ class PongGame(Widget):
             # bounce ball off bottom or top
             if (self.ball.y < self.y * 0.9) or (self.ball.top > self.top * 0.9):
                 self.ball.velocity_y *= -1
+            
 
             # went of to a side to score point?
             if self.ball.x < self.x:
                 self.player2.score += 1
+                self.remove_boosts()
                 if self.player2.score == self.game_over_score:
                     self.game_over_state = True
                 else:
-                    self.serve_ball(vel=(4, 0))
+                    self.serve_ball(vel=(self.ball_speed, 0))
             if self.ball.right > self.width:
                 self.player1.score += 1
+                self.remove_boosts()
                 if self.player1.score == self.game_over_score:
                     self.game_over_state = True
                 else:
-                    self.serve_ball(vel=(4, 0))
-                self.serve_ball(vel=(-4, 0))
+                    self.serve_ball(vel=(self.ball_speed, 0))
+                self.serve_ball(vel=(-self.ball_speed, 0))
 
 
             # Movements on buttons
@@ -269,32 +331,26 @@ class PongGame(Widget):
         self.player1.score = 0
         self.player2.score = 0
         self.remove_boost_widget()
-        self.serve_ball(vel=(0,4))
+        self.remove_boosts()
+        self.serve_ball(vel=(self.ball_speed,0))
         self.change_menu_state()
         self.update_star()
+
+    def start_game(self):
+        self.start_widget.opacity = 0
+        self.start_state = False
+        self.serve_ball(vel=(self.ball_speed,0))
         
-    def on_menu_button_press(self):
-        self.menu_widget.opacity = 0
-        self.menu_state = False
-        print("Closing the menu")
-        print(self.menu_widget)
-
-    def on_menu_open(self):
-        self.menu_widget.opacity = 1
-        self.menu_state = True
-        print("Opening the Menu")
-        print(self.menu_widget)
-
     def change_menu_state(self):
-        if self.menu_widget.opacity == 0:
+        if not self.menu_state:
             self.menu_state = True
             self.menu_widget.opacity = 1
             print("Opening Menu")
-        elif self.menu_widget.opacity == 1:
+        elif self.menu_state:
             self.menu_state = False
             self.menu_widget.opacity = 0
             print("Closing Menu")
-            self.serve_ball(vel=(4,0))
+            self.serve_ball(vel=(self.ball_speed,0))
 
         return
 
--- menu.py
+++ menu.py
@@ -2,7 +2,14 @@ from kivy.uix.relativelayout import RelativeLayout
 
 
 class MenuWidget(RelativeLayout):
+    
     def on_touch_down(self, touch):
         if self.opacity == 0:
             return False
         return super(RelativeLayout, self).on_touch_down(touch)
+    
+class StartWidget(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.opacity ==0:
+            return False
+        return super().on_touch_down(touch)
